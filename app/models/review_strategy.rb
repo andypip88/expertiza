c23f9858d5734d0da321ba7e26f19b9dbaabc26a
Rachel Hyo Son <52179017+rachelhson@users.noreply.github.com>
2022-04-26 16:39:54 -0400

    Move TeamReviewStrategy & StudentReviewStrategy

--- app/models/review_strategy.rb
+++ app/models/review_strategy.rb
@@ -8,3 +8,33 @@ class ReviewStrategy
   end
 end
 
+class StudentReviewStrategy < ReviewStrategy
+  def reviews_per_team
+    (@participants.size * @review_num * 1.0 / @teams.size).round
+  end
+
+  def reviews_needed
+    @participants.size * @review_num
+  end
+
+  def reviews_per_student
+    @review_num
+  end
+end
+
+class TeamReviewStrategy < ReviewStrategy
+  def reviews_per_team
+    @review_num
+  end
+
+  def reviews_needed
+    @teams.size * @review_num
+  end
+
+  def reviews_per_student
+    (@teams.size * @review_num * 1.0 / @participants.size).round
+  end
+end
+
+
+
